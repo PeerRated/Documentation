189465ebba390dc08a3c24bf266b7475ff891b08
Taichen Rose <jatr812@gmail.com>
2025-09-04 09:29:50 -0700

    chore: Add project constants

---
+++ src/constants/application.ts
@@ -0,0 +1,5 @@
+export const ORG_NAME = "PeerRated";
+export const DOCUMENTATION_DESCRIPTION =
+  "This contains the documentation for the PeerRated project. It will include the business documents, technical documents, and user guide documentation.";
+export const DOCUMENTATION_GITHUB_REPO_URL =
+  "https://github.com/PeerRated/Documentation";
